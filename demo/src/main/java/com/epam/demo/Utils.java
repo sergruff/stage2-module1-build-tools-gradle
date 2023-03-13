1189c84dd22f6d9553ffe09251588082829435ea
sergruff <sergruff@gmail.com>
2023-03-13 22:07:36 +0100

    s2buildToolsGradle

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        if (args.isEmpty()){
+        if (args==null||args.isEmpty()){
             return false;
         }
         for (String s : args) {
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,12 +1,14 @@
 package com.epam.utils;
 
+import static org.apache.commons.lang3.math.NumberUtils.createNumber;
 import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (isCreatable(str)) {
+        if (!isCreatable(str)) {
             return false;
         }
-        return !str.contains("-") && !str.equals("0") && !str.trim().equals("");
+        return createNumber(str).doubleValue()>0;
+//                !str.contains("-") && !str.equals("0") && !str.trim().equals("");
     }
 }
